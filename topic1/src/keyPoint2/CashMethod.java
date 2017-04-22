0f7d04f6a906822bf374ecf3ac6ef30bb8d73962
federc <fede.ag@hotmail.com.ar>
2017-04-21 22:53:13 -0300

    key point 2 finished

---
+++ topic1/src/keyPoint2/CashMethod.java
@@ -0,0 +1,21 @@
+package keyPoint2;
+
+import java.util.ArrayList;
+
+public class CashMethod implements PaymentMethod {// I must apply Iterator pattern, but i can't figure it out how to do it (relying on the design pattern documentation)
+	
+
+	@Override
+	public void pay(ArrayList<ShoppingCartLine> shoppingCartLineCollection) {
+		System.out.println("paid with cash --> 90% discount of the most expensive item");
+		int sum = 0;
+		for (int index = 0; index < shoppingCartLineCollection.size() ; index++){
+			// save the value of the most expensive item in a variable
+			//delete most expensive item
+			// then, calculate the total as: sum += (price*quant)+ variable that contains the value*0.1;	
+		}
+		
+		System.out.println("you get: "+ sum);
+	}
+
+}
---
+++ topic1/src/keyPoint2/ShoppingCart.java
@@ -0,0 +1,51 @@
+package keyPoint2;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+	
+	public ShoppingCart(){
+		
+	}
+	
+	private Client client;
+	private ShoppingCartLine shoppingCartLine;
+	private ArrayList<ShoppingCartLine> shoppingCartLineCollection;
+	
+	
+	public void setShoppingCartLine(ShoppingCartLine shoppingCartLine) {
+		this.shoppingCartLine = shoppingCartLine;
+	}
+
+	 
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public ArrayList<ShoppingCartLine> getShoppingCartLine() {
+		return shoppingCartLineCollection;
+	}
+
+	public void setShoppingCartLine(ArrayList<ShoppingCartLine> shoppingCartLine) {
+		this.shoppingCartLineCollection = shoppingCartLineCollection;
+	}
+
+	public ShoppingCart(Client client){
+		shoppingCartLineCollection = new ArrayList<ShoppingCartLine>();
+		
+		
+	}
+	
+	public void addShoppingCartLine(ShoppingCartLine SCL){
+		shoppingCartLineCollection.add(SCL);
+	}
+	
+	
+	
+	
+}
