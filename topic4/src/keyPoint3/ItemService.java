4a5f00386736225604339116b1204f779100a50b
federc <fede.ag@hotmail.com.ar>
2017-05-06 12:15:42 -0300

    comment

--- topic4/src/keyPoint3/ItemService.java
+++ topic4/src/keyPoint3/ItemService.java
@@ -2,21 +2,27 @@ package keyPoint3;
 
 import java.util.List;
 
+import keyPoint2.Item;
+
 public interface ItemService {
-	
-	/** 
+
+	/**
 	 * This method it's called for retrieve all existing items
+	 * 
 	 * @author Aguirre Federico working on Java Bootcamp 2017
 	 * @return an item collection
-	 */		
+	 */
 	public List<Item> getAllItems();
-	
-	/** 
-	 * This method receives an item name. Then, searches and returns the specific item
+
+	/**
+	 * This method receives an item name. Then, it searches and returns the
+	 * specific item
+	 * 
 	 * @author Aguirre Federico working on Java Bootcamp 2017
-	 * @param receives an item name
+	 * @param receives
+	 *            an item name
 	 * @return a specific item
-	 */	
+	 */
 	public Item getItemByName(String name);
 
 }
